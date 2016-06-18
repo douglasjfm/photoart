ab8c54362e8b6a200e8dec5cd380ce9f5fe98777
root <root@localhost.localdomain>
2016-06-17 23:39:49 -0300

    fim 2. add funcao ler todo bd

--- server.php
+++ server.php
@@ -5,18 +5,48 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$fotos = $_POST['value'];
+if (isset($_GET['all']))
+    lerBD();
+else
+{
+    $fotos = $_POST['value'];
 
-$i = 0;
-$table = '<table class="relat"><th>Link<th>Nome</th><th>Comentario</th>';
-foreach ($fotos as $f) {
-    $link = $f["link"];
-    $nome = $f["nome"];
-    $com = $f["comentario"];
-    $conn->query("INSERT INTO `photos` (link,nome,comentario) VALUES ('$link','$nome','$com')");
+    $i = 0;
+    $table = '<table class="relat"><th>Link<th>Nome</th><th>Comentario</th>';
+    foreach ($fotos as $f) {
+        $link = $f["link"];
+        $nome = $f["nome"];
+        $com = $f["comentario"];
+        $conn->query("INSERT INTO `photos` (link,nome,comentario) VALUES ('$link','$nome','$com')");
 
-    $table .= "<tr>"."<td>".$link."</td>"."<td>".$nome."</td>"."<td>".$com."</td>"."</tr>";
+        $table .= "<tr>"."<td>".$link."</td>"."<td>".$nome."</td>"."<td>".$com."</td>"."</tr>";
+    }
+    $table .= "</table>";
+    echo $table;
 }
-$table .= "</table>";
-echo $table;
+
+function lerBD()
+{
+    global $conn;
+    $r = $conn->query("SELECT * FROM `photos`");
+    $fotos = [];
+    $i = 0;
+    while ($row = $r->fetch_assoc()) {
+        $fotos[$i] = $row;
+        $i++;
+    }
+
+    $i = 0;
+    $table = '<link rel="stylesheet" type="text/css" href="app.css"><table class="relat"><th>Link<th>Nome</th><th>Comentario</th>';
+    foreach ($fotos as $f) {
+        $link = $f["link"];
+        $nome = $f["nome"];
+        $com = $f["comentario"];
+        $conn->query("INSERT INTO `photos` (link,nome,comentario) VALUES ('$link','$nome','$com')");
+
+        $table .= "<tr>"."<td>".$link."</td>"."<td>".$nome."</td>"."<td>".$com."</td>"."</tr>";
+    }
+    $table .= "</table>";
+    echo $table;
+}
 ?>
